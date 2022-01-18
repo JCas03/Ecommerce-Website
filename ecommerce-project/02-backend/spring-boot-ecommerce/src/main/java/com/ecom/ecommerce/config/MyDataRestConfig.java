275e5980cbdc9507a65d0629bc99b9a516653d45
juancastorena <03juan.c.castorena@gmail.com>
2022-01-18 14:21:17 -0600

    Proj. Beginning

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/ecom/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/ecom/ecommerce/config/MyDataRestConfig.java
@@ -2,11 +2,13 @@ package com.ecom.ecommerce.config;
 
 import com.ecom.ecommerce.entity.Product;
 import com.ecom.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
