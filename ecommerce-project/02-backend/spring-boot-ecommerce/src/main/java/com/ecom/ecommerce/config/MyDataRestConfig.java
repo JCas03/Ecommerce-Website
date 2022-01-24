345c7143726011c41afcd1cff532754b39dadbc6
juancastorena <03juan.c.castorena@gmail.com>
2022-01-24 15:41:06 -0600

    Front-End

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/ecom/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/ecom/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,30 @@ package com.ecom.ecommerce.config;
 
 import com.ecom.ecommerce.entity.Product;
 import com.ecom.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
@@ -24,5 +40,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        // call helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        //expose entity ids
+
+        // get list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // create array of the entity type
+        List<Class> entityClasses = new ArrayList<>();
+
+        // get entity types for the entities
+        for(EntityType tempEntityType : entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+        // expose entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,13 +3,18 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
+import { ProductCategoryMenuComponent } from '../components/product-category-menu/product-category-menu.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+
   private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category'
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,14 +23,25 @@ export class ProductService {
     //Build URL based on cat. id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
-    )
+    );
+  }
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductsCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
   }
 }
 
-interface GetResponse{
+interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductsCategory{
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
