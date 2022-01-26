1dbf7fa721240a4c9f091ed8897ecb5236e56ac6
juancastorena <03juan.c.castorena@gmail.com>
2022-01-26 11:34:29 -0600

    Section 15

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -6,9 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { Routes, RouterModule, Router} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { ProductDetailsComponent } from './componets/product-details/product-details.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
 
 const routes: Routes = [
+  {path: 'product/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'product', component: ProductListComponent},
@@ -21,6 +25,7 @@ const routes: Routes = [
     AppComponent,
     ProductListComponent,
     ProductCategoryMenuComponent,
+    SearchComponent,
     ProductDetailsComponent
   ],
   imports: [
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -10,13 +10,18 @@ import { ProductCategoryMenuComponent } from '../components/product-category-men
   providedIn: 'root'
 })
 export class ProductService {
-
-
-  private baseUrl = 'http://localhost:8080/api/products';
+    private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category'
 
   constructor(private httpClient: HttpClient) { }
+
+  getProduct(theProductId: number): Observable<Product> {
+    //call URL based on Prod. ID
+    const productUrl = `${this.baseUrl}/${theProductId}`
+
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
