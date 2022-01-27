8daf4216686e983b50507890519c3e3efd1d6252
juancastorena <03juan.c.castorena@gmail.com>
2022-01-27 12:46:49 -0600

    Ecom

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -23,6 +23,15 @@ export class ProductService {
 
     return this.httpClient.get<Product>(productUrl);
   }
+  getProductListPaginate(thePage: number, 
+                        thePageSize: number,
+                        theCategoryId: number): Observable<GetResponseProducts> {
+
+    //Build URL based on cat. id, page and size
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}` + `&page=${thePage}&size=${thePageSize}`
+
+     return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
@@ -51,6 +60,13 @@ export class ProductService {
 interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  },
+  page:{
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
+
   }
 }
 
